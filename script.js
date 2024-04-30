5acb9f781e21f135e9df17c925c61648ca0a5469
Kexaty_ <eliasbenjaminserrudo@gmail.com>
2024-04-30 20:53:09 -0300

    v1.0

---
+++ script.js
@@ -0,0 +1,25 @@
+function verifi() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "elias" && password == "123") {
+        alert("¡Inicio de sesión correcto!");
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+}
+
+function toggleMenu() {
+    var menu = document.querySelector('.link-nav');
+    menu.classList.toggle('menu-abierto');
+}
+
+
+
+let cantidadCarrito = 0;
+
+        function agregarAlCarrito() {
+            cantidadCarrito++;
+            console.log("Producto agregado al carrito. Cantidad actual: " + cantidadCarrito);
+            document.getElementById("cantidad-carrito").textContent = cantidadCarrito;
+        }
